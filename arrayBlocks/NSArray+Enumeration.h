4b206c99aef17d433a52cf55e46eaf74ddcce178
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-12 11:00:53 -0500

    Creates NSArray+Enumeration category and stubs out methods.

---
+++ arrayBlocks/NSArray+Enumeration.h
@@ -0,0 +1,14 @@
+//
+//  NSArray+Enumeration.h
+//  arrayBlocks
+//
+//  Created by Ken M. Haggerty on 2/12/16.
+//  Copyright © 2016 Zachary Drossman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Enumeration)
+- (NSArray *)arrayByApplyingBlock:(void(^)(id obj, NSUInteger idx, BOOL *stop))block;
+- (NSArray *)elementsSatisfyingBlock:(BOOL(^)(id evaluatedObject, NSDictionary *bindings))block;
+@end
---
+++ arrayBlocks/NSArray+Enumeration.m
@@ -0,0 +1,25 @@
+//
+//  NSArray+Enumeration.m
+//  arrayBlocks
+//
+//  Created by Ken M. Haggerty on 2/12/16.
+//  Copyright © 2016 Zachary Drossman. All rights reserved.
+//
+
+#import "NSArray+Enumeration.h"
+
+@implementation NSArray (Enumeration)
+
+- (NSArray *)arrayByApplyingBlock:(void(^)(id obj, NSUInteger idx, BOOL *stop))block
+{
+    
+    return nil;
+}
+
+- (NSArray *)elementsSatisfyingBlock:(BOOL(^)(id evaluatedObject, NSDictionary *bindings))block
+{
+    
+    return nil;
+}
+
+@end
